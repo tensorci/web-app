9947191d572842c1f7fc40179e22365e23ad67d6
Ben Whittle <benwhittle31@gmail.com>
2018-01-13 16:10:08 -0500

    switching icon to TensorCZI

--- src/components/deployments/Logs.js
+++ src/components/deployments/Logs.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ajax from '../../utils/Ajax';
+import $ from 'jquery';
 
 class Logs extends Component {
 
@@ -20,18 +21,61 @@ class Logs extends Component {
       follow: true
     };
 
-    Ajax.get('/api/deployment/logs', payload)
-      .then((resp) => {
-        return resp.body.getReader().read();
-      }).then((result, done) => {
-        if (done) {
-          // ???
-        } else {
-          // do something with each chunk
-          console.log(result);
-          this.setState({ logs: result });
-        }
-      });
+    // fetch('/api/logtest').then(response => {
+    //   // response.body is a readable stream.
+    //   // Calling getReader() gives us exclusive access to
+    //   // the stream's content
+    //   var reader = response.body.getReader();
+    //   var bytesReceived = 0;
+    //
+    //   // read() returns a promise that resolves
+    //   // when a value has been received
+    //   return reader.read().then(function processResult(result) {
+    //     // Result objects contain two properties:
+    //     // done  - true if the stream has already given
+    //     //         you all its data.
+    //     // value - some data. Always undefined when
+    //     //         done is true.
+    //     if (result.done) {
+    //       console.log("Fetch complete");
+    //       return;
+    //     }
+    //
+    //     // result.value for fetch streams is a Uint8Array
+    //     bytesReceived += result.value.length;
+    //     console.log('Received', bytesReceived, 'bytes of data so far');
+    //
+    //     // Read some more, and call this function again
+    //     return reader.read().then(processResult);
+    //   });
+    // });
+
+    // Ajax.get('/api/logtest', payload)
+    // fetch('/api/logtest', { headers: { 'Content-Type': 'application/octet-stream' } })
+    //   .then((resp) => {
+    //     var pump = (reader) => {
+    //       return reader.read().then((result) => {
+    //         console.log(result);
+    //
+    //         if (result.done) {
+    //           return;
+    //         }
+    //
+    //         var chunk = result.value;
+    //         var text = '';
+    //
+    //         for (var i = 0; i < chunk.byteLength; i++) {
+    //           text += String.fromCharCode(chunk[i]);
+    //         }
+    //
+    //         $('#fuck').html(text);
+    //
+    //         return pump(reader);
+    //       });
+    //     };
+    //
+    //     return pump(resp.body.getReader());
+    //   });
   }
 
   getLogs() {
@@ -48,7 +92,7 @@ class Logs extends Component {
     return (
       <div className="log-container card">
         <div className="action-log-messages">
-          <pre className="output">{this.getLogs()}</pre>
+          <pre className="output" id="fuck">{this.getLogs()}</pre>
         </div>
       </div>
     );
