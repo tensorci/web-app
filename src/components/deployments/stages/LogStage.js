754907045137f9506a706e4408ad9efe46e6c895
Ben Whittle <benwhittle31@gmail.com>
2018-01-20 02:46:59 -0800

    adding jquery-backed log height transitions

--- src/components/deployments/stages/LogStage.js
+++ src/components/deployments/stages/LogStage.js
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class LogStage extends Component {
 
   constructor(props) {
     super(props);
 
-    this.getLogComp = this.getLogComp.bind(this);
     this.getLogs = this.getLogs.bind(this);
+    this.toggleHeight = this.toggleHeight.bind(this);
 
     this.state = this.props.data || {};
     this.state.current = this.props.current || false;
-    this.state.open = true; // TODO: switch to '= this.state.current;' --> just hardcoding for now.
     this.state.success = true;
-  }
-
-  getLogComp() {
-    if (!this.state.open) {
-      return;
-    }
-
-    return (
-      <div className={'detail contents' + (this.state.success ? ' success' : '')}>
-        <div className="action-log-messages">
-          <pre className="output">
-            <span className="pre">{this.getLogs()}</span>
-          </pre>
-        </div>
-      </div>
-    );
   }
 
   getLogs() {
@@ -36,20 +20,41 @@ class LogStage extends Component {
     });
   }
 
+  toggleHeight() {
+    const $parent = $(this.parent);
+    const $el = $(this.wrapperRef);
+
+    if ($parent.hasClass('open')) {
+      $parent.removeClass('open');
+      $el.animate({ height: 0 }, 200 );
+    } else {
+      $parent.addClass('open');
+      $el.animate({ height: $el.get(0).scrollHeight }, 200 );
+    }
+  }
+
   render() {
     return (
       <div className="build-output">
-        <div className={'action-header contents' + (this.state.open ? ' open' : '') + (this.state.success ? ' success' : '')}>
+        <div className={'action-header contents' + (this.state.current ? ' open' : '') + (this.state.success ? ' success' : '')} ref={(r) => { this.parent = r; }}>
           <div className="ah-wrapper">
             <div className="header contents">
               <div className="button contents">
-                <i className="fa fa-chevron-right right-arrow" onClick={() => { this.setState({ open: !this.state.open }); }}></i>
+                <i className="fa fa-chevron-right right-arrow" onClick={this.toggleHeight}></i>
               </div>
               <div className="command contents">
                 <span className="stage-name">{this.state.name}</span>
               </div>
             </div>
-            <div className="detail-wrapper">{this.getLogComp()}</div>
+            <div className="detail-wrapper" ref={(r) => { this.wrapperRef = r; }}>
+              <div className={'detail contents' + (this.state.success ? ' success' : '')}>
+                <div className="action-log-messages">
+                  <pre className="output">
+                    <span className="pre">{this.getLogs()}</span>
+                  </pre>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
