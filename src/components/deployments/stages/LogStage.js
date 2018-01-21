77d69eb29d0b4f1d4c02ae731a4508a0ab98b0f4
Ben Whittle <benwhittle31@gmail.com>
2018-01-21 13:22:03 -0800

    attempting log height fix

--- src/components/deployments/stages/LogStage.js
+++ src/components/deployments/stages/LogStage.js
@@ -56,7 +56,7 @@ class LogStage extends Component {
 
     return (
       <div className="build-output">
-        <div className={'action-header contents open' + this.getStatusClass(data, current)} ref={(r) => { this.actionHeaderRef = r; }}>
+        <div className={'action-header contents' + (detailWrapperStyle.height === 'auto' ? ' open' : '') + this.getStatusClass(data, current)} ref={(r) => { this.actionHeaderRef = r; }}>
           <div className="ah-wrapper">
             <div className="ah-wrapper-header contents" onClick={this.toggleHeight}>
               <div className="button contents">
