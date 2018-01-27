1b6dbc34beb321612c65ad0df374a3bedd20e1f2
Ben Whittle <benwhittle31@gmail.com>
2018-01-26 21:21:26 -0800

    adding errMsg to Ajax custom

--- src/utils/Ajax.js
+++ src/utils/Ajax.js
@@ -12,37 +12,58 @@
  */
 
 import $ from 'jquery';
+import banner from './Banner';
 import Session from './Session';
 
 var Ajax;
 
 class AjaxClass {
 
-  get(url, params) {
-    return this.urlEncoded(url, params, 'GET');
+  constructor() {
+    this.get = this.get.bind(this);
+    this.post = this.post.bind(this);
+    this.put = this.put.bind(this);
+    this.delete = this.delete.bind(this);
+    this.urlEncoded = this.urlEncoded.bind(this);
+    this.jsonRequst = this.jsonRequst.bind(this);
+    this.defaultHeaders = this.defaultHeaders.bind(this);
+    this.requestSucceeded = this.requestSucceeded.bind(this);
   }
 
-  post(url, params) {
-    return this.jsonRequst(url, params, 'POST');
+  get(url, params, errMsg) {
+    return this.urlEncoded(url, params, 'GET', errMsg);
   }
 
-  put(url, params) {
-    return this.jsonRequst(url, params, 'PUT');
+  post(url, params, errMsg) {
+    return this.jsonRequst(url, params, 'POST', errMsg);
   }
 
-  delete(url, params) {
-    return this.urlEncoded(url, params, 'DELETE');
+  put(url, params, errMsg) {
+    return this.jsonRequst(url, params, 'PUT', errMsg);
   }
 
-  urlEncoded(url, params, method) {
+  delete(url, params, errMsg) {
+    return this.urlEncoded(url, params, 'DELETE', errMsg);
+  }
+
+  urlEncoded(url, params, method, errMsg) {
     if (params) {
       url += ('?' + $.param(params));
     }
 
-    return fetch(url, { method: method, headers: this.defaultHeaders() });
+    return fetch(url, {
+      method: method,
+      headers: this.defaultHeaders()
+    }).then((resp) => {
+      if (this.requestSucceeded(resp)) {
+        return resp.json();
+      } else if (errMsg) {
+        banner.error(errMsg);
+      }
+    });
   }
 
-  jsonRequst(url, params, method) {
+  jsonRequst(url, params, method, errMsg) {
     var headers = this.defaultHeaders();
     headers['Content-Type'] = 'application/json';
 
@@ -50,6 +71,12 @@ class AjaxClass {
       method: method,
       headers: headers,
       body: JSON.stringify(params || {})
+    }).then((resp) => {
+      if (this.requestSucceeded(resp)) {
+        return resp.json();
+      } else if (errMsg) {
+        banner.error(errMsg);
+      }
     });
   }
 
@@ -61,6 +88,10 @@ class AjaxClass {
     }
 
     return headers;
+  }
+
+  requestSucceeded(resp) {
+    return resp.status === 200 || resp.status === 201;
   }
 }
 
